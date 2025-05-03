3921dd3dd2939139afcf15741db397fa7a701c95
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-03 10:07:45 +0300

    feat: update type definitions for User, Conversation, and Message in db.d.ts

--- types/db.d.ts
+++ types/db.d.ts
@@ -1,6 +1,19 @@
 import {
+  Conversation as ConversationType,
   OauthAccount as OauthAccountType,
   User as UserType,
 } from "@prisma/client";
+export type User = UserType & {
+  oauthAccounts?: OauthAccount[];
+  conversations?: Conversation[];
+  messages?: Message[];
+};
 export type OauthAccount = OauthAccountType & { user?: UserType };
-export type User = UserType & { oauthAccounts?: OauthAccount[] };
+export type Conversation = ConversationType & {
+  users?: User[];
+  messaages?: Message[];
+};
+export type Message = MessageType & {
+  sender?: User;
+  conversation?: Conversation;
+};
